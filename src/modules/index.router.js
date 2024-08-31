0f3b96dd676615ec97713d7b857562ae1e26bedf
Esraa Ragab <esraargbali@gmail.com>
2024-08-31 20:15:23 +0300

    update

--- src/modules/index.router.js
+++ src/modules/index.router.js
@@ -18,9 +18,6 @@ export const appRouter = (app)=>{
 app.use(cors(
   {
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 200,
   }
 ))
 
